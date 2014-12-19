482c812901d1b9f377c81bb4681445831b910163
Alon Diamant <alon@everything.me>
2014-12-19 07:46:00 +0200

    Blind attempt for cached

--- firebase/__init__.py
+++ firebase/__init__.py
@@ -104,3 +104,58 @@ class Firebase():
     def __url(self):
         #We append .json to end of ROOT_URL for REST API.
         return '%s.json' % self.ROOT_URL
+
+# TODO: Make caching persistent
+_GlobalCache = {}
+
+class CachedFirebase(Firebase):
+    _firebase = None
+
+    def __init__(self, root_url, auth_token=None):
+        self._firebase = Firebase(root_url, auth_token)
+
+    #These methods are intended to mimic Firebase API calls.
+
+    def child(self, path):
+        root_url = '%s/' % self.ROOT_URL
+        url = urlparse.urljoin(root_url, str(path).lstrip('/'))
+        return CachedFirebase(url, auth_token=self.auth_token)
+
+    def parent(self):
+        url = os.path.dirname(self.ROOT_URL)
+        #If url is the root of your Firebase, return None
+        up = urlparse.urlparse(url)
+        if up.path == '':
+            return None #maybe throw exception here?
+        return CachedFirebase(url, auth_token=self.auth_token)
+
+    #These mirror REST API functionality
+
+    def put(self, data):
+        global _GlobalCache
+        _GlobalCache[self.ROOT_URL] = None
+        return self.__request('put', data = data)
+
+    def patch(self, data):
+        global _GlobalCache
+        _GlobalCache[self.ROOT_URL] = None
+        return self.__request('patch', data = data)
+
+    def get(self):
+        global _GlobalCache
+        if not self.ROOT_URL in _GlobalCache:
+            _GlobalCache[self.ROOT_URL] = self.__request('get')
+        return _GlobalCache[self.ROOT_URL]
+
+    #POST differs from PUT in that it is equivalent to doing a 'push()' in
+    #Firebase where a new child location with unique name is generated and
+    #returned
+    def post(self, data):
+        global _GlobalCache
+        _GlobalCache[self.ROOT_URL] = None
+        return self.__request('post', data = data)
+
+    def delete(self):
+        global _GlobalCache
+        _GlobalCache[self.ROOT_URL] = None
+        return self.__request('delete')
